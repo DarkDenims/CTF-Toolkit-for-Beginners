c4834208e0862ae9597eee7606a7c370e65536a6
Xavier Romano <xavieromano1027@gmail.com>
2023-08-18 21:33:11 +0800

    I'm starting from scratch because using bash scripts to make a checklist is, incredibly hard. might make it easier with python

--- install.sh
+++ install.sh
@@ -1,61 +1,23 @@
 #!/bin/bash
 
-# Output color variables
-RED='\033[0;31m'
-NC='\033[0m'
+# Choose distribution
+echo "Choose your Linux distribution:"
+read -p "1. Debian-based (Parrot/Kali), 2. Arch-based: " distribution
 
-# Choose which distro
-echo -e "${RED}[*] Choose your Linux distribution:${NC}"
-options=("Debian-based (Parrot/Kali)" "Arch-based")
+case $distribution in
+    1)
+        echo "Debian (Parrot/Kali) OS Customization Script"
+        sudo apt-get update
+        ;;
+    2)
+        echo "Arch-based OS Customization Script"
+        sudo pacman -Syyu
+        ;;
+    *)
+        echo "Invalid option"
+        exit 1
+        ;;
+esac
 
-"""
-Will install via categories soon starting with
-Cryptography
--Cryptool
--Hashcat
-Networking
--tcpdump
--nmap
-Web
--dirbuster
--gobuster
-Steganography
--steghide
--stegsolve
-Exploit Development
--pwntools
--johntheripper
--metasploit
-OSINT
--Sherlock
-Misc
--PayloadsAllTheThings
--Seclists
--hydra
--rockyou
-"""
-
-
-select opt in "${options[@]}"; do
-    case $opt in
-        "Debian-based (Parrot/Kali)")
-            echo -e "${RED}[*] Debian (Parrot/Kali) OS Customization Script${NC}"
-            # Update Repositories for Debian-based distro
-            echo -e "${RED}[*] Updating repo list${NC}"
-            sudo apt-get update
-
-            # wget https://github.com/jcryptool/core/releases/download/1.0.9/JCrypTool-1.0.9-Linux-64bit.tar.gz
-            break
-            ;;
-        "Arch-based")
-            echo -e "${RED}[*] Arch-based OS Customization Script${NC}"
-            # Update Repositories for Arch-based distro
-            echo -e "${RED}[*] Updating repo list${NC}"
-            sudo pacman -Syyu
-            break
-            ;;
-        *)
-            echo -e "${RED}Invalid option${NC}"
-            ;;
-    esac
-done
+# Call the Python script
+python3 install_packages.py
